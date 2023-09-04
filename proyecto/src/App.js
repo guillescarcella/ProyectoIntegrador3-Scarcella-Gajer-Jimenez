faa7e8c47d97880d148b77c631f048a98423d271
guillescarcella <scarcellag@udesa.edu.ar>
2023-09-04 11:29:56 -0300

    seguimos home

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,11 +1,16 @@
+import {Switch, Route} from "react-router-dom"
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import NotFound from "./screens/NotFound/NotFound";
-import Home from './screens/Home'
+import Home from './screens/Home/Home'
 
 function App() {
   return (
+    <>
+    <Header/>
     <Home/>
+    <Footer/>
+    </>
   );
 }
  
--- proyecto/src/components/Movie/Movie.js
+++ proyecto/src/components/Movie/Movie.js
@@ -1,10 +1,26 @@
 import {Component} from "react"
+import './styles.css';
+import {Link} from "react-router-dom";
 
 class Movie extends Component {
     constructor(props){
         super(props)
         this.state={
         } // aca va lo de favoritos
+    }
+
+    render(){
+        return(
+            <div className="pelicula">
+                <Link to={`/movie/id/${this.props.id}`}>
+                <img src={this.props.imagen} alt={this.props.nombre}/>
+                </Link>
+                    <h3 className="titulos"> {this.props.nombre}</h3>
+                    <h5 className="descripcion"> {this.props.descripcion}</h5>
+            </div>
+            
+            
+        )
     }
 }
 
