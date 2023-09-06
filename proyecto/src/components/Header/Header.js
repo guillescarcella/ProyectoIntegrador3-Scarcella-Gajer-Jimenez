2fad781fadbf8d5f7d8af39cdf85851da350e7f5
taligajer <tgajer@udesa.edu.ar>
2023-09-06 08:22:33 -0300

    links header

--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -11,14 +11,14 @@ class Header extends Component{
 
     render() {
         return(
+            
             <nav className="header">
-                <img className= "imagen"src="./logo512" alt="logo"></img>
-                <ul>
-                    <li> <Link className="navbar" to="/home" exact="true">Home</Link> </li> 
-                    <li> <Link className="navbar" to="/favoritos">Favoritos</Link> </li> 
-                    <li> <Link className="navbar" to="/peliculas">Peliculas</Link> </li> 
-                    <li> <Link className="navbar" to="/series">Series</Link> </li> 
-                </ul>
+            <img className= "imagen"src="./logo512" alt="logo"></img>
+                <a className='link' href="/home">Home</a>
+                <a className='link' href="/peliculas">Peliculas </a>
+                <a className='link' href="/series"> Series</a>
+                <a className='link' href="/favoritos">Favoritos</a>
+                  
             </nav>
         )
     }
