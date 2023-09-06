ed174d77d653ebe0fcedcfdb706d98bfead309f2
taligajer <tgajer@udesa.edu.ar>
2023-09-06 10:49:34 -0300

    links header

--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './styles.css';
 
 class Header extends Component{
@@ -14,18 +15,24 @@ class Header extends Component{
             
             <nav className="header">
             <img className= "imagen"src="./logo512" alt="logo"></img>
-                <a className='link' href="/home">Home</a>
-                <a className='link' href="/peliculas">Peliculas </a>
-                <a className='link' href="/series"> Series</a>
-                <a className='link' href="/favoritos">Favoritos</a>
+            <ul >
+                <li> <Link className="link" to="/home" exact="estrenos">Home</Link></li> 
+                <li> <Link className="link" to="/favoritos">Favoritos</Link></li> 
+                <li> <Link className="link" to="/series">Series</Link></li> 
+                <li> <Link className="link" to="/peliculas">Peliculas</Link></li> 
+            </ul>
                 <form onSubmit={(event)=>this.evitarSubmit(event)}>
                     <label>Buscador</label>
                     <input type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} />
                     <input type="submit" value="Submit" />
                 </form>     
+
             </nav>
         )
     }
 }
 
 export default Header
+
+
+
