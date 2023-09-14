c08f1ac5a2ee178e4dfb0d11c53122b8b017ddc3
guillescarcella <scarcellag@udesa.edu.ar>
2023-09-14 13:09:03 -0300

    cargar mas

--- proyecto/src/screens/Peliculas/Peliculas.js
+++ proyecto/src/screens/Peliculas/Peliculas.js
@@ -7,6 +7,8 @@ import MoviesContainer from '../../components/MoviesContainer/MoviesContainer'
         super(props)
         this.state ={
             movies: [], 
+            backup: [],
+            page:1
         }
     }
 
@@ -14,16 +16,37 @@ componentDidMount(){
     fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options) //poner todas las peliculas
         .then(response => response.json())
         .then(data => this.setState({
-            movies: data.results   
+            movies: data.results,
+            backup: data.results  
         }))
         
         .catch(err => console.error(err));
 }
+TraerMasMovies(){
+    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${this.state.page + 1}&sort_by=popularity.desc`, options)
+    .then(response => response.json())
+    .then(data=> this.setState({
+        movies: this.state.movies.concat(data.results),
+        backup: this.state.backup.concat(data.results),
+        page: this.state.page + 1
+    }))
+}
+
+filtrarMovies(nombre){
+    let moviesFiltradas = this.state.backup.filter((elm) => elm.title.toLowerCase().includes(nombre.toLowerCase()))
+    this.setState({
+        movies: moviesFiltradas
+    })
+}
+
 render(){
     return(
         <div>
             <h1 className='barras'> PELICULAS </h1>
+            {/* FALTA EL FORM DE BUSCAR PELICULAS */}
             <MoviesContainer movies={this.state.movies}/>
+            <button onClick={()=> this.TraerMasMovies()}> Cargar mas peliculas </button>
+            
         </div>
     )
 }
--- proyecto/src/screens/Series/Series.js
+++ proyecto/src/screens/Series/Series.js
@@ -6,7 +6,9 @@ import SeriesContainer from '../../components/SeriesContainer/SeriesContainer'
     constructor(props){
         super(props)
         this.state ={ 
-            series:[]
+            series:[],
+            backup:[],
+            page:1
         }
     }
 
@@ -14,15 +16,36 @@ componentDidMount(){
     fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc', options)
         .then(response => response.json())
         .then(data => this.setState({
-            series: data.results
+            series: data.results,
+            backup:data.results
         }))
         .catch(err => console.error(err));
 }
+
+TraerMasSeries(){
+    fetch(`https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=${this.state.page + 1}&sort_by=popularity.desc`, options)
+    .then(response => response.json())
+    .then(data=> this.setState({
+        series: this.state.series.concat(data.results),
+        backup: this.state.backup.concat(data.results),
+        page: this.state.page + 1
+    }))
+}
+
+filtrarSeries(nombre){
+    let seriesFiltradas = this.state.backup.filter((elm) => elm.title.toLowerCase().includes(nombre.toLowerCase()))
+    this.setState({
+        series: seriesFiltradas
+    })
+}
+
 render(){
     return(
         <div>
             <h1 className='barras'> SERIES </h1>
+            {/* FALTA EL FORM DE BUSCAR PELICULAS */}
             <SeriesContainer series={this.state.series}/>
+            <button onClick={()=> this.TraerMasSeries()}> Cargar mas series  </button>
         </div>
     )
 }
